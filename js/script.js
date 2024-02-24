36e77497f8f8b4d6382b001a85dec8ccd8d00142
Jad Bou Raad <we.code.fs@gmail.com>
2024-02-24 11:24:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+`use strict`;
+
+function runTypingEffect() {
+  const text = "I am the author";
+  const typingElement = document.querySelector("#typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
